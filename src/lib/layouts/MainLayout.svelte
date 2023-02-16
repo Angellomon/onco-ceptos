2d6b5f90b172fa62173a6e15ccb0592f33cae40f
Angellomon <angelmtzdiaz@gmail.com>
2023-02-16 17:36:51 -0600

    feat(styles): impl series listing

--- src/lib/layouts/MainLayout.svelte
+++ src/lib/layouts/MainLayout.svelte
@@ -1,9 +1,21 @@
 <script>
   import Header from "../components/Header.svelte";
-  import Showcase from "../components/Showcase.svelte";
+  import SeriesListing from "../components/listing/SeriesListing.svelte";
+  import Showcase from "../components/showcase/Showcase.svelte";
 </script>
 
 <main>
   <Header />
   <Showcase />
+  <SeriesListing />
 </main>
+
+<style>
+  main {
+    min-width: 100vw;
+    margin: 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+</style>
