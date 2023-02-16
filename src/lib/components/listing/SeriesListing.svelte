8f2f9ced4e2e3675b62ffe0b03b0d47657a10099
Angellomon <angelmtzdiaz@gmail.com>
2023-02-16 17:31:33 -0600

    feat(lib/components): impl listing components

---
+++ src/lib/components/listing/SeriesListing.svelte
@@ -0,0 +1,18 @@
+<section />
+
+<style>
+  section {
+    background-color: var(--sidebar-color);
+
+    display: flex;
+    flex-direction: row;
+
+    height: 100%;
+    padding: 0;
+    margin: 0;
+
+    min-height: 70vh;
+
+    position: relative;
+  }
+</style>
---
+++ src/lib/components/listing/SeriesListingSidebar.svelte
@@ -0,0 +1,9 @@
+<div class="series-navbar" />
+
+<style>
+  div.series-navbar {
+    position: absolute;
+
+    height: 100%;
+  }
+</style>
