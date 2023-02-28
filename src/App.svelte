207f00a8aff0c7e4fb3155fcb4b9d6e5356f5b51
Angellomon <angelmtzdiaz@gmail.com>
2023-02-28 11:14:56 -0600

    feat(app): rearrange layoyts

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
+  import { loadSeriesData } from "./lib/utils";
+  import { episodesStore, seriesStore, showcaseStore } from "./lib/store";
   import MainLayout from "./lib/layouts/MainLayout.svelte";
+
+  let ready = false;
+
+  onMount(async () => {
+    const data = await loadSeriesData();
+
+    episodesStore.set(data.episodes);
+    seriesStore.set(data.series);
+    showcaseStore.set(data.showcase);
+
+    ready = true;
+  });
 </script>
 
-<MainLayout />
+{#if ready}
+  <MainLayout />
+{:else}
+  <p>cargando</p>
+{/if}
 
 <style>
 </style>
