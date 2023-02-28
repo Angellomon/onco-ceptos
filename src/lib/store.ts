d166156c982c4dba55b0534ebfdf8e97b7f5b4d4
Angellomon <angelmtzdiaz@gmail.com>
2023-02-28 11:18:44 -0600

    feat(lib/store): impl selected series store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,10 @@
 import { writable } from "svelte/store";
-import type { EpisodeType, SeriesType, ShowcaseType } from "../types/showcase";
+import type { EpisodeType, SeriesType, ShowcaseType } from "../types/series";
 
 export const showcaseStore = writable<ShowcaseType[]>([]);
 
 export const seriesStore = writable<SeriesType[]>([]);
 
 export const episodesStore = writable<EpisodeType[]>([]);
+
+export const selectedSeries = writable<SeriesType | null>(null);
