c032cb722b98fb9bef2d9a3fad05d3b3cceb8b0d
Angellomon <angelmtzdiaz@gmail.com>
2023-02-28 11:12:08 -0600

    feat(types): impl typing

---
+++ src/types/series.ts
@@ -0,0 +1,20 @@
+export type SeriesType = {
+  title: string; // * also works as ID
+  description?: string;
+  shortDescription?: string;
+};
+
+export type EpisodeType = {
+  title: string;
+  seriesTitle: string;
+  episodeNumber: number;
+  description?: string;
+  videoUrl: string;
+  previewUrl?: string;
+};
+
+export type ShowcaseType = {
+  title: string;
+  description?: string;
+  previewUrl?: string;
+};
