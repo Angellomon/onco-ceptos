77b99adac912c12251da131478ca5022b95227f1
Angellomon <angelmtzdiaz@gmail.com>
2023-03-01 17:26:08 -0600

    chore(lib/components): improve episode descirptiosn

--- src/lib/components/EpisodeDescription.svelte
+++ src/lib/components/EpisodeDescription.svelte
@@ -1,6 +1,19 @@
+<script lang="ts">
+  import { selectedEpisode } from "../store";
+</script>
+
 <div class="episode-description">
-  <slot />
+  {#if $selectedEpisode}
+    <h2>{$selectedEpisode.title}</h2>
+  {:else}
+    <h3>No encontrado :(</h3>
+  {/if}
 </div>
 
 <style>
+  div.episode-description {
+    background-color: var(--primary-color);
+    filter: brightness(50%);
+    width: 100%;
+  }
 </style>
