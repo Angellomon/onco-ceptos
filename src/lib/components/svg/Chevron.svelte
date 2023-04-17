1a7516fafe36da9d5777caf6d8026d725bb705e3
Angellomon <angelmtzdiaz@gmail.com>
2023-04-17 15:31:05 -0600

    feat(assets/svg): impl icons

---
+++ src/lib/components/svg/Chevron.svelte
@@ -0,0 +1,24 @@
+<script>
+  import ChevronSVG from "../../../assets/chevron-down-solid.svg";
+
+  export let width = 15;
+
+  export let marginLeft = 10;
+  export let marginRight = 10;
+
+  export let hover = false;
+
+  const style = `margin-left: ${marginLeft}px; margin-right: ${marginRight}px;`;
+</script>
+
+<img class:hover src={ChevronSVG} {width} alt=">" {style} />
+
+<style>
+  img {
+    transition: all 0.2s ease-in-out;
+  }
+
+  img.hover {
+    transform: scale(1.3);
+  }
+</style>
---
+++ src/lib/components/svg/Search.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import SearchSVG from "../../../assets/magnifying-glass-solid.svg";
+
+  const dispatch = createEventDispatcher();
+
+  export let width = 20;
+
+  function handleClick() {
+    return () => {
+      dispatch("click");
+    };
+  }
+</script>
+
+<img
+  src={SearchSVG}
+  {width}
+  alt="/"
+  on:click={handleClick()}
+  on:keypress={() => {}}
+/>
+
+<style>
+  img {
+    position: absolute;
+    left: 15px;
+    top: 25%;
+  }
+</style>
