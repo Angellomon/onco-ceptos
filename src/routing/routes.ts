d5262235d706302f281ce95827790c5228b93230
Angellomon <angelmtzdiaz@gmail.com>
2023-04-17 03:38:33 -0600

    feat(routing: impl new)

---
+++ src/routing/routes.ts
@@ -0,0 +1,7 @@
+// import EpisodeLayout from "./lib/layouts/EpisodeLayout.svelte";
+import MainLayout from "../lib/layouts/MainLayout.svelte";
+
+export const routes = {
+  "/": MainLayout,
+  // "/episodio/:title": EpisodeLayout,
+};
