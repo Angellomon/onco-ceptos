fa7186a6d3c0fc93077b87511714a59f5baaf4ec
Angellomon <angelmtzdiaz@gmail.com>
2023-04-17 15:33:23 -0600

    feat(lib/components/search): impl search components

--- src/lib/components/season-search/SeasonSearch.svelte
+++ src/lib/components/season-search/SeasonSearch.svelte
@@ -1,23 +1,48 @@
 <script lang="ts">
   import { searchText } from "../../store";
+  import Search from "../svg/Search.svelte";
+
+  function handleClick() {
+    return () => {
+      console.log($searchText);
+    };
+  }
 </script>
 
-<input placeholder="Buscar" type="text" bind:value={$searchText} />
+<div class="search">
+  <Search on:click={handleClick()} />
+  <input placeholder="Buscar" type="text" bind:value={$searchText} />
+</div>
 
 <style>
+  div.search {
+    --font-size: 20px;
+    --input-left-padding: 50px;
+
+    --search-width: 100%;
+    --search-height: 30px;
+
+    align-self: center;
+    position: relative;
+    padding-left: 0;
+  }
+
   input {
     appearance: none;
     border-radius: 5px;
-    height: 30px;
+    height: var(--search-height);
+    /* width: var(--search-width); */
 
     align-self: center;
-    border: 1px solid var(--dark-background);
+    border: 3px solid rgba(0, 0, 0, 0.1);
 
     margin-right: 20px;
 
     padding: 5px 10px;
 
-    font-size: 18px;
+    padding-left: var(--input-left-padding);
+
+    font-size: var(--font-size);
   }
 
   input:focus,
