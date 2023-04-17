a6235a8caabf96b18842dd6887c9df2bba00a912
Angellomon <angelmtzdiaz@gmail.com>
2023-04-17 17:13:25 -0600

    feat(assets/svg): impl logo component

---
+++ src/lib/components/svg/OnconceptosLogo.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ColoredLogo from "../../../assets/logo-onconceptos_color.svg";
+  import WhiteLogo from "../../../assets/logo-onconceptos_blanco.svg";
+
+  export let type: "color" | "white" = "color";
+  export let width = 150;
+  export let paddingLeft = 50;
+  export let alt = "onconceptos";
+
+  let style = `padding-left: ${paddingLeft}px`;
+</script>
+
+<img {width} {style} {alt} src={type == "white" ? WhiteLogo : ColoredLogo} />
