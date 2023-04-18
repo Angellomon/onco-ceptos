fd6e8b048985a0790b868ec657749bdb5e41cdee
Angellomon <angelmtzdiaz@gmail.com>
2023-04-17 19:30:34 -0600

    feat(lib/layout/main): impl Title component

--- src/lib/layouts/MainLayout.svelte
+++ src/lib/layouts/MainLayout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Header from "../components/Header.svelte";
+  import Title from "../components/Title.svelte";
   import { dataIsLoading } from "../store";
   import Loading from "./Loading.svelte";
 </script>
@@ -9,5 +10,6 @@
 {:else}
   <main>
     <Header />
+    <Title />
   </main>
 {/if}
