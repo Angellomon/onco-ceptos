30473e5746613e4c472619a1b3e2044c8e81b33c
Angellomon <angelmtzdiaz@gmail.com>
2023-04-18 16:43:44 -0600

    feat(lib/components/svg): impl svg components

---
+++ src/lib/components/svg/ArrowRight.svelte
@@ -0,0 +1,23 @@
+<script>
+  import svgSource from "../../../assets/flecha-adelante.svg";
+
+  export let width = 20;
+
+  export let hover = false;
+</script>
+
+<img class:hover {width} src={svgSource} alt=">" />
+
+<style>
+  img {
+    position: absolute;
+    left: 15px;
+    top: 35%;
+
+    transition: all 0.2s ease-in-out;
+  }
+
+  img.hover {
+    transform: scale(1.3);
+  }
+</style>
---
+++ src/lib/components/svg/Plus.svelte
@@ -0,0 +1,21 @@
+<script>
+  import plusSVGSource from "../../../assets/simbolo-mas.svg";
+
+  export let width = 20;
+  export let hover = false;
+</script>
+
+<img class:hover {width} src={plusSVGSource} alt="+" />
+
+<style>
+  img {
+    position: absolute;
+    left: 15px;
+    top: 30%;
+
+    transition: all 0.2s ease-in-out;
+  }
+  img.hover {
+    transform: scale(1.3);
+  }
+</style>
