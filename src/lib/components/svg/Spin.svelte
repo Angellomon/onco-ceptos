df00b78373d66b5695597756455f5123090c0470
Angellomon <angelmtzdiaz@gmail.com>
2023-04-17 19:29:06 -0600

    feat(lib/components/svg): impl Spin component

---
+++ src/lib/components/svg/Spin.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Spin from "../../../assets/spin-onconceptos.svg";
+
+  export let width = 200;
+  export let bottom = 0;
+  export let right = 0;
+
+  let style = `bottom: ${bottom}px; right: ${right}px;`;
+</script>
+
+<img {width} {style} src={Spin} alt="cargando..." />
+
+<style>
+  img {
+    position: absolute;
+  }
+</style>
