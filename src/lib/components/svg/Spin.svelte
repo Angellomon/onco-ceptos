01f2b7628e03c44c4fdc67286be0c65f2deebc8e
Angellomon <angelmtzdiaz@gmail.com>
2023-04-17 19:48:20 -0600

    feat(style): transitions

--- src/lib/components/svg/Spin.svelte
+++ src/lib/components/svg/Spin.svelte
@@ -1,14 +1,22 @@
 <script>
+  import { fly, slide } from "svelte/transition";
   import Spin from "../../../assets/spin-onconceptos.svg";
 
   export let width = 200;
   export let bottom = 0;
   export let right = 0;
+  export let duration = 1000;
 
   let style = `bottom: ${bottom}px; right: ${right}px;`;
 </script>
 
-<img {width} {style} src={Spin} alt="cargando..." />
+<img
+  {width}
+  {style}
+  src={Spin}
+  alt="cargando..."
+  transition:fly={{ x: 300, duration }}
+/>
 
 <style>
   img {
