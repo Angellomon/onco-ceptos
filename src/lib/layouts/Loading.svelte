5281b3f92f954ccefbd67a17aa56d281dfea8658
Angellomon <angelmtzdiaz@gmail.com>
2023-04-17 19:29:33 -0600

    feat(lib/layout/loading): impl spin loading

--- src/lib/layouts/Loading.svelte
+++ src/lib/layouts/Loading.svelte
@@ -1,1 +1,21 @@
-<p>cargando...</p>
+<script lang="ts">
+  import { BarsRotateFade } from "svelte-svg-spinners";
+  import { fade } from "svelte/transition";
+</script>
+
+<section transition:fade>
+  <BarsRotateFade width={300} size={500} color="#ffffff" />
+</section>
+
+<style>
+  section {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+  }
+</style>
