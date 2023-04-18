512db75a781b6b8928aa7de5e499adea3dca7abe
Angellomon <angelmtzdiaz@gmail.com>
2023-04-18 16:11:01 -0600

    feat(lib/components/player): imple Episode Player

---
+++ src/lib/components/player/EpisodePlayer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Player, Video, DefaultControls, DefaultUi } from "@vime/svelte";
+  import { selectedEpisode, episodesStore } from "../../store";
+
+  export let widthPercent = 50;
+
+  $: episode = $selectedEpisode || $episodesStore[0];
+  $: playerStyle = `width: ${widthPercent}%;`;
+</script>
+
+<Player style={playerStyle}>
+  <Video>
+    <source data-src={episode.videoUrl} />
+  </Video>
+
+  <DefaultUi noControls>
+    <DefaultControls hideOnMouseLeave activeDuration={2000} />
+  </DefaultUi>
+</Player>
+
+<style>
+</style>
