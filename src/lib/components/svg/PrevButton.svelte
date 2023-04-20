c2e71917a6e1d9051072dbc0f016c8bdaccdee17
Angellomon <angelmtzdiaz@gmail.com>
2023-04-20 02:07:14 -0600

    feat(lib/components/svg): impl prev/next buttons

---
+++ src/lib/components/svg/PrevButton.svelte
@@ -0,0 +1,47 @@
+<script>
+  import prevButtonSVGSource from "../../../assets/boton-avance_izquierda.svg";
+
+  export let width = 20;
+  let hover = false;
+
+  function handleMouseOver() {
+    return () => {
+      hover = true;
+    };
+  }
+
+  function handleMouseLeave() {
+    return () => {
+      hover = false;
+    };
+  }
+</script>
+
+<img
+  class:hover
+  {width}
+  on:mouseover={handleMouseOver()}
+  on:mouseleave={handleMouseLeave()}
+  on:focus={() => {}}
+  src={prevButtonSVGSource}
+  alt="<"
+/>
+
+<style>
+  img {
+    /* position: absolute; */
+
+    transition: all 0.2s ease-in-out;
+
+    cursor: pointer;
+
+    height: 100%;
+    width: 55px;
+
+    z-index: 10;
+  }
+
+  img.hover {
+    transform: scale(1.2);
+  }
+</style>
