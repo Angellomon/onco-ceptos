4cfc1690a795addc45ad69b67dc98617c705da35
Angellomon <angelmtzdiaz@gmail.com>
2023-04-19 19:48:10 -0600

    feat(lib/components): impl episode compoentn

---
+++ src/lib/components/seasons-listing/Episode.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { EpisodeType } from "../../../types/series";
+
+  export let episode: EpisodeType;
+</script>
+
+<div id={episode.id} class="episode-box">
+  {episode.title}
+</div>
+
+<style>
+  div.episode-box {
+  }
+</style>
