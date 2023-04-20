d762b700c74a8eccde3af0e0525126288cb7f85c
Angellomon <angelmtzdiaz@gmail.com>
2023-04-19 19:52:49 -0600

    chore(style): fix gap

--- src/lib/layouts/MainLayout.svelte
+++ src/lib/layouts/MainLayout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Footer from "../components/Footer.svelte";
   import Header from "../components/Header.svelte";
   import SeasonsListing from "../components/seasons-listing/SeasonsListing.svelte";
   import SelectedEpisode from "../components/SelectedEpisode.svelte";
@@ -12,8 +13,9 @@
 {:else}
   <main>
     <Header />
-    <Title />
     <SelectedEpisode />
     <SeasonsListing />
+    <Title />
+    <Footer />
   </main>
 {/if}
