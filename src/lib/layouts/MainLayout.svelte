1f254772f61a3414dd3501bcbf1140e5862ed156
Angellomon <angelmtzdiaz@gmail.com>
2023-04-24 15:52:05 -0600

    feat(layouts/main): impl modal

--- src/lib/layouts/MainLayout.svelte
+++ src/lib/layouts/MainLayout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import Footer from "../components/Footer.svelte";
   import Header from "../components/Header.svelte";
+  import EpisodeInfoModal from "../components/episode-info-modal/EpisodeInfoModal.svelte";
   import SeasonsListing from "../components/seasons-listing/SeasonsListing.svelte";
   import SelectedEpisode from "../components/SelectedEpisode.svelte";
   import Title from "../components/Title.svelte";
   import { dataIsLoading } from "../store";
   import Loading from "./Loading.svelte";
+
+  let hidden = true;
 </script>
 
 {#if $dataIsLoading}
@@ -17,5 +20,18 @@
     <SeasonsListing />
     <Title />
     <Footer />
+    <EpisodeInfoModal bind:hidden />
   </main>
 {/if}
+
+<style>
+  main {
+    position: relative;
+  }
+
+  /* .scroll-lock {
+    overflow: hidden;
+    margin: 0;
+    height: 100%;
+  } */
+</style>
