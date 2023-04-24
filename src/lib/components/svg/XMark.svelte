cad8c2c60e53d0556fec7a7840016b4ff4bd0ca3
Angellomon <angelmtzdiaz@gmail.com>
2023-04-24 15:49:46 -0600

    feat(assets/close-button): impl svg + component

---
+++ src/lib/components/svg/XMark.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import xMarkSVGSource from "../../../assets/boton-cerrar.svg";
+
+  export let width = 30;
+  export let hover = false;
+
+  const dispatch = createEventDispatcher();
+
+  function handleCloseClick() {
+    dispatch("click");
+  }
+</script>
+
+<img
+  on:click={handleCloseClick}
+  on:keypress={() => {}}
+  class:hover
+  src={xMarkSVGSource}
+  alt="x"
+  {width}
+/>
+
+<style>
+  img {
+    position: absolute;
+    right: 15px;
+    top: 15px;
+
+    transition: all 0.2s ease-in-out;
+
+    filter: invert(10%) sepia(31%) saturate(2808%) hue-rotate(190deg)
+      brightness(93%) contrast(96%);
+    cursor: pointer;
+  }
+
+  img.hover {
+    transform: scale(1.3);
+  }
+</style>
