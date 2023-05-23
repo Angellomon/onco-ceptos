b00f5f29a29d6fc497943d50853ca9764a2244c3
Angellomon <angelmtzdiaz@gmail.com>
2023-05-23 11:17:20 -0600

    git(ignore): ignore types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
+const root = process.env.MSD_PATH || "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   optimizeDeps: {
     include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
   },
+  root,
 });
