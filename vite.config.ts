c5d655c1289df9089a38e98fba664ffb126fe744
Angellomon <angelmtzdiaz@gmail.com>
2023-05-24 16:25:07 -0600

    feat(config): build options

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,12 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  const base = process.env.MSD_PATH || "/";
-
-  console.log(base, mode);
+  // process.env = { ...process.env, ...loadEnv(mode, ".") };
 
   return defineConfig({
     plugins: [svelte()],
     optimizeDeps: {
       include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
-    },
-    server: {
-      base,
     },
   });
 };
