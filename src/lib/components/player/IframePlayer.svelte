6576e1181451fd64d11e4d4378d79b4863722243
Angellomon <angelmtzdiaz@gmail.com>
2023-06-01 17:15:10 -0600

    feat(lib/components): impl iframe video player

---
+++ src/lib/components/player/IframePlayer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { selectedEpisode } from "../../store";
+  export let width = 569;
+  export let height = 320;
+
+  $: src = $selectedEpisode.videoUrl;
+  $: title = $selectedEpisode.title;
+</script>
+
+<iframe {title} {width} {height} {src} allowfullscreen />
+
+<style>
+  iframe {
+    border: none;
+    margin-top: 30px;
+  }
+</style>
