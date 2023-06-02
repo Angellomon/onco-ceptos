fbb29a3c6daf505025614c46bff9d75e124b4e7a
Angellomon <angelmtzdiaz@gmail.com>
2023-06-02 15:44:23 -0600

    chore: warnings

--- src/lib/components/player/EpisodePlayer.svelte
+++ src/lib/components/player/EpisodePlayer.svelte
@@ -2,8 +2,6 @@
   import { Player, Video, DefaultControls, DefaultUi } from "@vime/svelte";
   import { selectedEpisode, episodesStore } from "../../store";
 
-  export let widthPercent = 50;
-
   $: episode = $selectedEpisode || $episodesStore[0];
   $: playerStyle = `width: 500px; margin-top: 5%`;
 </script>
