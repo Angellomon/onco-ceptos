d105a98990ea05e80dc3c0047546d18132c9f7ce
Angellomon <angelmtzdiaz@gmail.com>
2023-06-09 01:04:33 -0600

    feat(components/buttons): impl Quizz Button

--- src/lib/components/seasons-listing/SeasonItem.svelte
+++ src/lib/components/seasons-listing/SeasonItem.svelte
@@ -1,39 +1,13 @@
 <script lang="ts">
-  import { episodesStore } from "../../store";
   import type { SeasonType } from "../../../types/series";
-  import EpisodesListing from "./EpisodesListing.svelte";
-  import Interrogation from "../svg/Interrogation.svelte";
   import EpisodesGrid from "../episodes-grid/EpisodesGrid.svelte";
 
   export let season: SeasonType;
-
-  let hover = false;
-
-  function handleMouseOver() {
-    return () => {
-      hover = true;
-    };
-  }
-
-  function handleMouseLeave() {
-    return () => {
-      hover = false;
-    };
-  }
-  $: episodes = $episodesStore.filter((e) => e.seasonId == season.id);
 </script>
 
 <aside id={season.id}>
   <div class="season-box">
     <h3>{season.title}</h3>
-    <button
-      on:mouseover={handleMouseOver()}
-      on:mouseleave={handleMouseLeave()}
-      on:focus={() => {}}
-      class="quizz"
-    >
-      <Interrogation {hover} /> Quizz</button
-    >
   </div>
   <div class="episodes">
     <!-- <EpisodesListing {episodes} /> -->
@@ -95,25 +69,6 @@
     color: white;
 
     font-size: 35px;
-  }
-
-  button.quizz {
-    border: 2px solid white;
-    border-radius: var(--btn-border-radius);
-
-    padding: 5px 20px;
-    padding-left: 40px;
-
-    height: 50px;
-
-    position: relative;
-
-    background: var(--secondary-color);
-    color: white;
-
-    font-size: 18px;
-
-    cursor: pointer;
   }
 
   /* button.info {
