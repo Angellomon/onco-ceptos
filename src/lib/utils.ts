b40f0778b95e344aa70ef4a2a4dbdf71d4f238d2
Angellomon <angelmtzdiaz@gmail.com>
2023-06-13 15:05:47 -0600

    feat(compat): iOS compat

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -368,7 +368,14 @@ export function verifyInstalledVersion() {
 }
 
 export function getDate(date: string) {
-  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+  console.log("determinig user agent");
+
+  // const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+  let isSafari =
+    navigator.vendor.match(/apple/i) &&
+    !navigator.userAgent.match(/crios/i) &&
+    !navigator.userAgent.match(/fxios/i) &&
+    !navigator.userAgent.match(/Opera|OPT\//);
 
   console.log("is safari", isSafari);
 
