3e69f574fab530bfe27e3d3e702c556472f48de9
Angellomon <angelmtzdiaz@gmail.com>
2023-06-13 14:25:13 -0600

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig, loadEnv } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   // process.env = { ...process.env, ...loadEnv(mode, ".") };
 
   return defineConfig({
-    plugins: [svelte()],
+    plugins: [
+      svelte(),
+      legacy({
+        targets: ["defaults", "iOS >= 13.2"],
+        polyfills: true,
+        modernPolyfills: true,
+      }),
+    ],
     optimizeDeps: {
       include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
-    },
-    build: {
-      target: ["edge88", "firefox78", "chrome87", "safari12"],
     },
   });
 };
