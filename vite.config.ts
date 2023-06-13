08b2c6ef3caf6724c0fdb24da7a01b73ea57ada6
Angellomon <angelmtzdiaz@gmail.com>
2023-06-13 13:44:10 -0600

    build

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,8 @@ export default ({ mode }) => {
     optimizeDeps: {
       include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
     },
+    build: {
+      target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
+    },
   });
 };
