308188586c10b37a1657b45eb015e32629387357
Angellomon <angelmtzdiaz@gmail.com>
2023-06-13 23:59:21 -0600

    feat(components/debug): impl debug component

---
+++ src/lib/components/debug/DebugData.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { dataCollectionErrorJson, registrationErrorJson } from "../../store";
+</script>
+
+<pre>data: {$dataCollectionErrorJson}</pre>
+<pre>registration: {$registrationErrorJson}</pre>
+
+<style>
+  pre {
+    background-color: rgba(235, 186, 52, 0.8);
+  }
+</style>
